c7bbf7467b1ae6ec9fe40a708a3fb85120d05168
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-04-10 15:45:51 +0300

    Bit utils

---
+++ contracts/protocol/libraries/math/BitUtils.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.12;
+
+import {SafeMath} from '../../../dependencies/openzeppelin/contracts/SafeMath.sol';
+import {WadRayMath} from './WadRayMath.sol';
+
+library BitUtils {
+  using SafeMath for uint256;
+  using WadRayMath for uint256;
+
+  function nextPowerOf2(uint256 v) internal pure returns (uint256) {
+    if (v == 0) {
+      return 1;
+    }
+    v--;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v |= v >> 32;
+    v |= v >> 64;
+    v |= v >> 128;
+    return v + 1;
+  }
+
+  function isPowerOf2(uint256 v) internal pure returns (bool) {
+    return (v & (v - 1)) == 0;
+  }
+
+  function bitLength(uint256 v) internal pure returns (uint256 len) {
+    if (v == 0) {
+      return 0;
+    }
+    if (v > type(uint128).max) {
+      v >>= 128;
+      len += 128;
+    }
+    if (v > type(uint64).max) {
+      v >>= 64;
+      len += 64;
+    }
+    if (v > type(uint32).max) {
+      v >>= 32;
+      len += 32;
+    }
+    if (v > type(uint16).max) {
+      v >>= 16;
+      len += 16;
+    }
+    if (v > type(uint8).max) {
+      v >>= 8;
+      len += 8;
+    }
+    if (v > 15) {
+      v >>= 4;
+      len += 4;
+    }
+    if (v > 3) {
+      v >>= 2;
+      len += 2;
+    }
+    if (v > 1) {
+      len += 1;
+    }
+    return len;
+  }
+}
