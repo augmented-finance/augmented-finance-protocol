b00e5c5eb3a14fc8259d04cc7329787726fb9891
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-04-16 22:01:07 +0300

    Harness fixes

--- specs/harness/StableDebtTokenHarness.sol
+++ specs/harness/StableDebtTokenHarness.sol
@@ -2,6 +2,8 @@ pragma solidity 0.6.12;
 
 import {StableDebtToken} from '../../contracts/protocol/tokenization/StableDebtToken.sol';
 import {IncentivizedERC20} from '../../contracts/protocol/tokenization/IncentivizedERC20.sol';
+import {ILendingPool} from '../../contracts/interfaces/ILendingPool.sol';
+import {IBalanceHook} from '../../contracts/interfaces/IBalanceHook.sol';
 
 contract StableDebtTokenHarness is StableDebtToken {
   constructor(
@@ -10,7 +12,9 @@ contract StableDebtTokenHarness is StableDebtToken {
     string memory name,
     string memory symbol,
     address incentivesController
-  ) public StableDebtToken(pool, underlyingAsset, name, symbol, incentivesController) {}
+  ) public {
+    StableDebtToken.initialize(ILendingPool(pool), underlyingAsset, IBalanceHook(incentivesController), 18, name, symbol, "");
+  }
 
   /**
    Simplification: The user accumulates no interest (the balance increase is always 0).
@@ -20,14 +24,14 @@ contract StableDebtTokenHarness is StableDebtToken {
   }
 
   function _calcTotalSupply(uint256 avgRate) internal view override returns (uint256) {
+    avgRate;
     return IncentivizedERC20.totalSupply();
   }
 
-  function getIncentivesController() public view returns (address) {
-    return address(_incentivesController);
-  }
-
   function rayWadMul(uint256 aRay, uint256 bWad) external view returns (uint256) {
+    aRay;
+    bWad;
+    this;
     return aRay.rayMul(bWad.wadToRay());
   }
 }
