8d855a50e7fffda72df5b903955c2fd5bb779522
agAgent011 <agAgent011@pm.me>
2021-04-16 23:20:26 +0300

    test reward

--- test-suites/test-augmented/rewards.spec.ts
+++ test-suites/test-augmented/rewards.spec.ts
@@ -4,7 +4,7 @@ import { makeSuite, TestEnv } from '../test-aave/helpers/make-suite';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 import rawBRE, { ethers } from 'hardhat';
 
-import { getRewardFreezer } from '../../helpers/contracts-getters';
+import { getLinearUnweightedRewardPool, getRewardFreezer } from '../../helpers/contracts-getters';
 import { RewardFreezer } from '../../types';
 
 chai.use(solidity);
@@ -32,6 +32,16 @@ describe('Migrator test suite', () => {
     rewardFreezer = await getRewardFreezer();
     expect(rewardFreezer.address).to.properAddress;
 
-    await rewardFreezer.admin_setFreezePortion(0);
+    const linearUnweightedRewardPool = await getLinearUnweightedRewardPool();
+    await linearUnweightedRewardPool.addRewardProvider(deployer.address); // instead token contract
+    await linearUnweightedRewardPool.setRate('1000000000000000000000000000');
+
+    await (await rewardFreezer.admin_setFreezePortion(0)).wait(1);
+
+    await linearUnweightedRewardPool.handleBalanceUpdate(deployer.address, 10, 20, 100);
+
+
+    const tx = await rewardFreezer.claimReward();
+    expect(tx.value).to.eq(0);
   });
 });
