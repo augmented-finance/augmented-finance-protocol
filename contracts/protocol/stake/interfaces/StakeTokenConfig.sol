8116bb8ba84245d6917d9136c314e5ed649aa72c
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-04-20 01:21:53 +0300

    Unified initialization for stake and voting tokens

---
+++ contracts/protocol/stake/interfaces/StakeTokenConfig.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.6.12;
+
+import {IERC20} from '../../../dependencies/openzeppelin/contracts/IERC20.sol';
+import {IBalanceHook} from '../../../interfaces/IBalanceHook.sol';
+import {IStakeAccessController} from './IStakeAccessController.sol';
+import {ITransferHook} from './ITransferHook.sol';
+
+struct StakeTokenConfig {
+  IStakeAccessController stakeController;
+  IERC20 stakedToken;
+  IBalanceHook incentivesController;
+  uint32 cooldownSeconds;
+  uint32 unstakeWindow;
+  // For voting token only
+  ITransferHook governance;
+}
