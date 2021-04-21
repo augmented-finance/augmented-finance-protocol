795b0cdd30a7fd16ae89a662fa231208a184c0be
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-04-21 17:33:49 +0300

    Extra check for proper upgradeability

--- contracts/mocks/upgradeability/MockStableDebtToken.sol
+++ contracts/mocks/upgradeability/MockStableDebtToken.sol
@@ -6,6 +6,6 @@ import {StableDebtToken} from '../../protocol/tokenization/StableDebtToken.sol';
 
 contract MockStableDebtToken is StableDebtToken {
   function getRevision() internal pure override returns (uint256) {
-    return 0x2;
+    return super.getRevision() + 1;
   }
 }
--- contracts/mocks/upgradeability/MockVariableDebtToken.sol
+++ contracts/mocks/upgradeability/MockVariableDebtToken.sol
@@ -6,6 +6,6 @@ import {VariableDebtToken} from '../../protocol/tokenization/VariableDebtToken.s
 
 contract MockVariableDebtToken is VariableDebtToken {
   function getRevision() internal pure override returns (uint256) {
-    return 0x2;
+    return super.getRevision() + 1;
   }
 }
