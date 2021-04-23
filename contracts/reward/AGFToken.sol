ba542bf5d6ab32d5d057520e309ab8a7a6b2791f
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-04-23 15:01:29 +0300

    Proper location of functions

--- contracts/reward/AGFToken.sol
+++ contracts/reward/AGFToken.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.12;
 
-import {IRemoteAccessBitmask} from '../access/interfaces/IRemoteAccessBitmask.sol';
+import {AccessFlags} from '../access/AccessFlags.sol';
+import {RemoteAccessBitmask} from '../access/RemoteAccessBitmask.sol';
+import {
+  IRemoteAccessBitmask,
+  RemoteAccessBitmaskHelper
+} from '../access/interfaces/IRemoteAccessBitmask.sol';
 
 import {RewardToken} from './RewardToken.sol';
 import {VersionedInitializable} from '../tools/upgradeability/VersionedInitializable.sol';
@@ -9,14 +14,23 @@ import {IInitializableRewardToken} from './interfaces/IInitializableRewardToken.
 
 import 'hardhat/console.sol';
 
-contract AGFToken is RewardToken, VersionedInitializable, IInitializableRewardToken {
+contract AGFToken is
+  RewardToken,
+  VersionedInitializable,
+  IInitializableRewardToken,
+  RemoteAccessBitmask
+{
   string internal constant NAME = 'Augmented Finance Reward Token';
   string internal constant SYMBOL = 'AGF';
   uint8 internal constant DECIMALS = 18;
 
   uint256 private constant TOKEN_REVISION = 1;
 
-  constructor() public RewardToken(IRemoteAccessBitmask(0), NAME, SYMBOL, DECIMALS) {}
+  constructor()
+    public
+    RewardToken(NAME, SYMBOL, DECIMALS)
+    RemoteAccessBitmask(IRemoteAccessBitmask(0))
+  {}
 
   function getRevision() internal pure virtual override returns (uint256) {
     return TOKEN_REVISION;
@@ -28,9 +42,34 @@ contract AGFToken is RewardToken, VersionedInitializable, IInitializableRewardTo
     string calldata symbol
   ) external virtual override initializerRunAlways(TOKEN_REVISION) {
     super._initializeERC20(name, symbol, DECIMALS);
-    super._initializeToken(remoteAcl);
+    _remoteAcl = remoteAcl;
     if (!isRevisionInitialized(TOKEN_REVISION)) {
       super._initializeDomainSeparator();
     }
+  }
+
+  function mint(address account, uint256 amount) external override aclHas(AccessFlags.REWARD_MINT) {
+    _mint(account, amount);
+  }
+
+  function burn(address account, uint256 amount) external aclHas(AccessFlags.REWARD_BURN) {
+    _burn(account, amount);
+  }
+
+  function _checkTransfer(address from, address to) internal view virtual {
+    require(_getRemoteAcl(from) & AccessFlags.REWARD_SUSPEND_ADDRESS == 0, 'sender is suspended');
+    if (from == to) {
+      return;
+    }
+    require(_getRemoteAcl(to) & AccessFlags.REWARD_SUSPEND_ADDRESS == 0, 'receiver is suspended');
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal virtual override {
+    super._beforeTokenTransfer(from, to, amount);
+    _checkTransfer(from, to);
   }
 }
--- contracts/reward/RewardToken.sol
+++ contracts/reward/RewardToken.sol
@@ -1,53 +1,15 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.12;
 
-import {Ownable} from '../dependencies/openzeppelin/contracts/Ownable.sol';
-import {SafeMath} from '../dependencies/openzeppelin/contracts/SafeMath.sol';
-import {Context} from '../dependencies/openzeppelin/contracts/Context.sol';
 import {ERC20WithPermit} from '../misc/ERC20WithPermit.sol';
-import {BitUtils} from '../tools/math/BitUtils.sol';
-import {AccessFlags} from '../access/AccessFlags.sol';
-import {RemoteAccessBitmask} from '../access/RemoteAccessBitmask.sol';
-import {IRemoteAccessBitmask} from '../access/interfaces/IRemoteAccessBitmask.sol';
-
 import {IRewardMinter} from './interfaces/IRewardMinter.sol';
 
 import 'hardhat/console.sol';
 
-abstract contract RewardToken is ERC20WithPermit, RemoteAccessBitmask, IRewardMinter {
-  using BitUtils for uint256;
-
+abstract contract RewardToken is ERC20WithPermit, IRewardMinter {
   constructor(
-    IRemoteAccessBitmask remoteAcl,
     string memory name,
     string memory symbol,
     uint8 decimals
-  ) public ERC20WithPermit(name, symbol, decimals) RemoteAccessBitmask(remoteAcl) {}
-
-  function _initializeToken(IRemoteAccessBitmask remoteAcl) internal {
-    _remoteAcl = remoteAcl;
-  }
-
-  function mint(address account, uint256 amount) external override aclHas(AccessFlags.REWARD_MINT) {
-    _mint(account, amount);
-  }
-
-  function burn(address account, uint256 amount) external aclHas(AccessFlags.REWARD_BURN) {
-    _burn(account, amount);
-  }
-
-  function _beforeTokenTransfer(
-    address from,
-    address to,
-    uint256
-  ) internal virtual override {
-    require(
-      _getRemoteAcl(from).hasNoneOf(AccessFlags.REWARD_SUSPEND_ADDRESS),
-      'sender is suspended'
-    );
-    require(
-      _getRemoteAcl(to).hasNoneOf(AccessFlags.REWARD_SUSPEND_ADDRESS),
-      'receiver is suspended'
-    );
-  }
+  ) public ERC20WithPermit(name, symbol, decimals) {}
 }
