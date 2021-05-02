972ade58e9d4b6b77b73ca614a13382fe1f2fb9d
agAgent011 <agAgent011@pm.me>
2021-05-02 19:01:14 +0300

    Revert "update tenderly"
    This reverts commit 6d11a8f18974a530c07eadb65840b15cd374bae3.

--- tasks/misc/set-bre.ts
+++ tasks/misc/set-bre.ts
@@ -18,17 +18,17 @@ task(`set-DRE`, `Inits the DRE, to have access to all the plugins' objects`).set
       console.log('- Setting up Tenderly provider');
       if (process.env.TENDERLY_FORK_ID && process.env.TENDERLY_HEAD_ID) {
         console.log('- Connecting to a Tenderly Fork');
-        _DRE.tenderlyNetwork.setFork(process.env.TENDERLY_FORK_ID);
-        _DRE.tenderlyNetwork.setHead(process.env.TENDERLY_HEAD_ID);
+        _DRE.tenderlyRPC.setFork(process.env.TENDERLY_FORK_ID);
+        _DRE.tenderlyRPC.setHead(process.env.TENDERLY_HEAD_ID);
       } else {
         console.log('- Creating a new Tenderly Fork');
-        await _DRE.tenderlyNetwork.initializeFork();
+        await _DRE.tenderlyRPC.initializeFork();
       }
-      const provider = new _DRE.ethers.providers.Web3Provider(_DRE.tenderlyNetwork as any);
+      const provider = new _DRE.ethers.providers.Web3Provider(_DRE.tenderlyRPC as any);
       _DRE.ethers.provider = provider;
       console.log('- Initialized Tenderly fork:');
-      console.log('  - Fork: ', _DRE.tenderlyNetwork.getFork());
-      console.log('  - Head: ', _DRE.tenderlyNetwork.getHead());
+      console.log('  - Fork: ', _DRE.tenderlyRPC.getFork());
+      console.log('  - Head: ', _DRE.tenderlyRPC.getHead());
       console.log('  - First account:', await (await _DRE.ethers.getSigners())[0].getAddress());
       console.log(
         '  - Balance:',
