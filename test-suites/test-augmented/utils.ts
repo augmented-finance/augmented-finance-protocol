ce84d181caa1bcc77cc115cc0a14ab826868a43a
agAgent0xFF <serge@augmented.finance>
2021-05-04 23:41:06 +0300

    wip: mining and unlock tests

--- test-suites/test-augmented/utils.ts
+++ test-suites/test-augmented/utils.ts
@@ -6,12 +6,22 @@ export const increaseTimeAndMine = async (secondsToIncrease: number) => {
   await ethers.provider.send('evm_mine', []);
 };
 
+export const mineToBlock = async (to: number) => {
+  const blk = await ethers.provider.getBlock('latest');
+  while (blk.number < to) {
+    blk.timestamp += 1;
+    await ethers.provider.send('evm_mine', [blk.timestamp]);
+    blk.number += 1;
+  }
+  console.log(`moved to block: ${await ethers.provider.getBlock('latest')}`);
+};
+
 export const mineBlocks = async (to: number) => {
   for (let i = 0; i < to; i++) {
     const res = await ethers.provider.getBlock('latest');
-    const currTime = new BigNumber(res.timestamp);
-    await ethers.provider.send('evm_mine', [currTime.toNumber() + 1]);
+    await ethers.provider.send('evm_mine', [res.timestamp + 1]);
   }
+  console.log(`moved to block: ${await ethers.provider.getBlock('latest')}`);
 };
 
 export const currentBlock = async () => {
