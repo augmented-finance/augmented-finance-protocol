204cbe735175c7d4df99b9c6b9151aad764715e8
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-05-10 13:05:55 +0300

    Split OpenZeppelin types to address linearization

---
+++ contracts/dependencies/openzeppelin/contracts/ERC20Events.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.6.12;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface ERC20Events {
+  /**
+   * @dev Emitted when `value` tokens are moved from one account (`from`) to
+   * another (`to`).
+   *
+   * Note that `value` may be zero.
+   */
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  /**
+   * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+   * a call to {approve}. `value` is the new allowance.
+   */
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
