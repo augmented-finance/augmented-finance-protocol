39e4b5233064df8bae958374d5db0344e826706f
agAgent0xFF <serge@augmented.finance>
2021-05-11 17:28:50 +0300

    wip

--- tasks/dev/9_augmented_migrator.ts
+++ tasks/dev/9_augmented_migrator.ts
@@ -16,6 +16,7 @@ import { waitForTx } from '../../helpers/misc-utils';
 export const ADAI_ADDRESS = '0x028171bca77440897b824ca71d1c56cac55b68a3';
 export const CDAI_ADDRESS = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643';
 export const DAI_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f';
+export const LP_ADDRESS = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9';
 
 task('dev:augmented-migrator', 'Deploy Augmented Migrator contracts.')
   .addOptionalParam('aDaiAddress', 'AAVE DAI address', ADAI_ADDRESS, types.string)
--- test-suites/test-augmented/migrator.spec.ts
+++ test-suites/test-augmented/migrator.spec.ts
@@ -2,11 +2,9 @@ import chai from 'chai';
 import { solidity } from 'ethereum-waffle';
 import { makeSuite, TestEnv } from '../test-aave/helpers/make-suite';
 import { Migrator } from '../../types';
-import { RAY } from '../../helpers/constants';
 import rawBRE, { ethers } from 'hardhat';
 import { getAToken, getMigrator } from '../../helpers/contracts-getters';
 import { ADAI_ADDRESS } from '../../tasks/dev/9_augmented_migrator';
-import BigNumber from 'bignumber.js';
 import { SignerWithAddress } from './helpers/make-suite';
 import { Signer } from 'ethers';
 import { convertToCurrencyDecimals } from '../../helpers/contracts-helpers';
@@ -32,15 +30,14 @@ makeSuite('Migrator test suite', (testEnv: TestEnv) => {
   });
 
   it('deposit and migrate aDai', async () => {
-    const referral = 101;
-    const aDaiAmount = await convertToCurrencyDecimals(ADAI_ADDRESS, '1000');
-    // impersonate mainnet aDAI, mint for user1, try deposit
+    const bigHolder = '0x4deb3edd991cfd2fcdaa6dcfe5f1743f6e7d16a6';
     await rawBRE.network.provider.request({
       method: 'hardhat_impersonateAccount',
-      params: [ADAI_ADDRESS],
+      params: [bigHolder],
     });
-    const aDaiContract = await getAToken(ADAI_ADDRESS);
-    await aDaiContract.connect(aDaiSigner).mint(user1.address, aDaiAmount, 1);
-    await m.connect(user1.signer).depositToMigrate(ADAI_ADDRESS, aDaiAmount, referral);
+    const bigHolderSigner = await ethers.getSigner(bigHolder);
+    const referral = 101;
+    const aDaiAmount = await convertToCurrencyDecimals(ADAI_ADDRESS, '10');
+    await m.connect(bigHolderSigner).depositToMigrate(ADAI_ADDRESS, aDaiAmount, referral);
   });
 });
