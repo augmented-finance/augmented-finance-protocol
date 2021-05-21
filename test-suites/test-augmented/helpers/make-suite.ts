a492d450a7c532a317acc271773eec388d4aa5cb
agAgent0xFF <serge@augmented.finance>
2021-05-21 16:33:23 +0300

    fix ag prefix in deploy

--- test-suites/test-augmented/helpers/make-suite.ts
+++ test-suites/test-augmented/helpers/make-suite.ts
@@ -128,9 +128,9 @@ export async function initializeMakeSuite() {
   testEnv.helpersContract = await getProtocolDataProvider();
 
   const allTokens = await testEnv.helpersContract.getAllATokens();
-  const aDaiAddress = allTokens.find((aToken) => aToken.symbol === 'aDAI')?.tokenAddress;
+  const aDaiAddress = allTokens.find((aToken) => aToken.symbol === 'agDAI')?.tokenAddress;
 
-  const aWEthAddress = allTokens.find((aToken) => aToken.symbol === 'aWETH')?.tokenAddress;
+  const aWEthAddress = allTokens.find((aToken) => aToken.symbol === 'agWETH')?.tokenAddress;
 
   const reservesTokens = await testEnv.helpersContract.getAllReservesTokens();
 
