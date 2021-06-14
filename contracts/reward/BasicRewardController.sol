000ddf7c3fd66c0badd03a153bf34cb4fb01e237
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-06-14 14:44:55 +0300

    Booster implementation w/o excess handling

--- contracts/reward/BasicRewardController.sol
+++ contracts/reward/BasicRewardController.sol
@@ -6,218 +6,26 @@ import {SafeMath} from '../dependencies/openzeppelin/contracts/SafeMath.sol';
 import {BitUtils} from '../tools/math/BitUtils.sol';
 
 import {IMarketAccessController} from '../access/interfaces/IMarketAccessController.sol';
-import {MarketAccessBitmask} from '../access/MarketAccessBitmask.sol';
-import {AccessFlags} from '../access/AccessFlags.sol';
-import {IManagedRewardController, AllocationMode} from './interfaces/IRewardController.sol';
-import {IRewardPool, IManagedRewardPool} from './interfaces/IRewardPool.sol';
 import {IRewardMinter} from '../interfaces/IRewardMinter.sol';
+import {BaseRewardController} from './BaseRewardController.sol';
 
 import 'hardhat/console.sol';
 
-abstract contract BasicRewardController is Ownable, MarketAccessBitmask, IManagedRewardController {
+abstract contract BasicRewardController is BaseRewardController {
   using SafeMath for uint256;
 
-  IRewardMinter private _rewardMinter;
-
-  IManagedRewardPool[] private _poolList;
-  /* IManagedRewardPool => mask */
-  mapping(address => uint256) private _poolMask;
-  /* holder => masks of related pools */
-  mapping(address => uint256) private _memberOf;
-
-  uint256 private _ignoreMask;
-  uint256 private _baselineMask;
-
-  bool private _paused;
-
-  constructor(IMarketAccessController accessController, IRewardMinter rewardMinter) public {
-    _remoteAcl = accessController;
-    _rewardMinter = rewardMinter;
-  }
-
-  event RewardsAllocated(address indexed user, uint256 amount);
-  event RewardsClaimed(address indexed user, address indexed to, uint256 amount);
-
-  function admin_addRewardPool(IManagedRewardPool pool) external override onlyOwner {
-    require(address(pool) != address(0), 'reward pool required');
-    require(_poolMask[address(pool)] == 0, 'already registered');
-    pool.claimRewardFor(address(this)); // access check
-    require(_poolList.length <= 255, 'too many pools');
-
-    uint256 poolMask = 1 << _poolList.length;
-    _poolMask[address(pool)] = poolMask;
-    _baselineMask |= poolMask;
-    _poolList.push(pool);
-  }
-
-  function admin_removeRewardPool(IManagedRewardPool pool) external override onlyOwner {
-    require(address(pool) != address(0), 'reward pool required');
-    uint256 mask = _poolMask[address(pool)];
-    if (mask == 0) {
-      return;
-    }
-    uint256 idx = BitUtils.bitLength(mask);
-    require(_poolList[idx] == pool, 'unexpected pool');
-
-    _poolList[idx] = IManagedRewardPool(0);
-    delete (_poolMask[address(pool)]);
-    _ignoreMask |= mask;
-  }
-
-  function admin_addRewardProvider(
-    address pool,
-    address provider,
-    address token
-  ) external onlyOwner {
-    IManagedRewardPool(pool).addRewardProvider(provider, token);
-  }
-
-  function admin_removeRewardProvider(address pool, address provider) external onlyOwner {
-    IManagedRewardPool(pool).removeRewardProvider(provider);
-  }
-
-  function updateBaseline(uint256 baseline) external override onlyOwner {
-    uint256 baselineMask = _baselineMask & ~_ignoreMask;
-
-    for (uint8 i = 0; i <= 255; i++) {
-      uint256 mask = uint256(1) << i;
-      if (mask & baselineMask == 0) {
-        if (mask > baselineMask) {
-          break;
-        }
-        continue;
-      }
-      if (_poolList[i].updateBaseline(baseline)) {
-        continue;
-      }
-      baselineMask &= ~mask;
-    }
-    _baselineMask = baselineMask;
-  }
-
-  function admin_setRewardMinter(IRewardMinter minter) external override onlyOwner {
-    _rewardMinter = minter;
-  }
-
-  function getPools() public view returns (IManagedRewardPool[] memory, uint256 ignoreMask) {
-    return (_poolList, _ignoreMask);
-  }
-
-  function getRewardMinter() external view returns (address) {
-    return address(_rewardMinter);
-  }
-
-  function claimReward() external returns (uint256 amount) {
-    // notPaused
-    return internalClaimAndMintReward(msg.sender, ~uint256(0), msg.sender);
-  }
-
-  function claimRewardAndTransferTo(address receiver, uint256 mask) external returns (uint256) {
-    // notPaused
-    require(receiver != address(0), 'receiver is required');
-    return internalClaimAndMintReward(msg.sender, mask, receiver);
-  }
-
-  function claimRewardFor(address holder, uint256 mask) external returns (uint256) {
-    // notPaused
-    require(holder != address(0), 'holder is required');
-    return internalClaimAndMintReward(holder, mask, holder);
-  }
-
-  function claimableReward(address holder, uint32 atBlock)
+  constructor(IMarketAccessController accessController, IRewardMinter rewardMinter)
     public
-    view
-    returns (uint256 claimable, uint256 delayed)
+    BaseRewardController(accessController, rewardMinter)
+  {}
+
+  function internalClaimAndMintReward(address holder, uint256 mask)
+    internal
+    override
+    returns (uint256 claimableAmount)
   {
-    return claimableRewardFor(holder, ~uint256(0), atBlock);
-  }
-
-  function claimableRewardFor(
-    address holder,
-    uint256 mask,
-    uint32 atBlock
-  ) public view returns (uint256 claimable, uint256 delayed) {
-    require(holder != address(0), 'holder is required');
-    if (atBlock == 0) {
-      atBlock = uint32(block.number);
-    } else {
-      require(atBlock >= uint32(block.number), 'should be zero, current or future block');
-    }
-    return internalCalcClaimableReward(holder, mask, atBlock);
-  }
-
-  function balanceOf(address holder) external view returns (uint256) {
-    if (holder == address(0)) {
-      return 0;
-    }
-    (uint256 claimable, uint256 delayed) =
-      internalCalcClaimableReward(holder, ~uint256(0), uint32(block.number));
-    return claimable.add(delayed);
-  }
-
-  function claimablePools(address holder) external view returns (uint256) {
-    return _memberOf[holder] & ~_ignoreMask;
-  }
-
-  function allocatedByPool(
-    address holder,
-    uint256 allocated,
-    uint32 sinceBlock,
-    AllocationMode mode
-  ) external override {
-    uint256 poolMask = _poolMask[msg.sender];
-    require(poolMask != 0, 'unknown pool');
-
-    if (allocated > 0) {
-      internalAllocatedByPool(holder, allocated, sinceBlock, uint32(block.number));
-      emit RewardsAllocated(holder, allocated);
-    }
-
-    if (mode == AllocationMode.Push) {
-      return;
-    }
-
-    uint256 pullMask = _memberOf[holder];
-    if (mode == AllocationMode.UnsetPull) {
-      if (pullMask & poolMask != 0) {
-        _memberOf[holder] = pullMask & ~poolMask;
-      }
-    } else {
-      if (pullMask & poolMask != poolMask) {
-        _memberOf[holder] = pullMask | poolMask;
-      }
-    }
-  }
-
-  function isRateController(address addr) public view override returns (bool) {
-    if (!hasRemoteAcl()) {
-      return addr == address(this);
-    }
-    return acl_hasAllOf(addr, AccessFlags.REWARD_RATE_ADMIN);
-  }
-
-  function isConfigurator(address addr) public view override returns (bool) {
-    return addr == owner();
-  }
-
-  function isEmergencyAdmin(address addr) public view override returns (bool) {
-    if (!hasRemoteAcl()) {
-      return addr == address(this);
-    }
-    return acl_hasAllOf(addr, AccessFlags.EMERGENCY_ADMIN);
-  }
-
-  function internalClaimAndMintReward(
-    address holder,
-    uint256 mask,
-    address receiver
-  ) private notPaused returns (uint256 claimableAmount) {
-    mask &= ~_ignoreMask;
-    mask &= _memberOf[holder];
-
     uint32 sinceBlock = 0;
     uint256 amountSince = 0;
-    uint32 currentBlock = uint32(block.number);
     bool incremental = false;
 
     for (uint256 i = 0; mask != 0; (i, mask) = (i + 1, mask >> 1)) {
@@ -225,7 +33,7 @@ abstract contract BasicRewardController is Ownable, MarketAccessBitmask, IManage
         continue;
       }
 
-      (uint256 amount_, uint32 since_) = _poolList[i].claimRewardFor(holder);
+      (uint256 amount_, uint32 since_) = getPool(i).claimRewardFor(holder);
       if (amount_ == 0) {
         continue;
       }
@@ -236,9 +44,7 @@ abstract contract BasicRewardController is Ownable, MarketAccessBitmask, IManage
       }
 
       if (amountSince > 0) {
-        claimableAmount = claimableAmount.add(
-          internalClaimByCall(holder, amountSince, sinceBlock, currentBlock)
-        );
+        claimableAmount = claimableAmount.add(internalClaimByCall(holder, amountSince, sinceBlock));
         incremental = true;
       }
       amountSince = amount_;
@@ -246,30 +52,18 @@ abstract contract BasicRewardController is Ownable, MarketAccessBitmask, IManage
     }
 
     if (amountSince > 0 || !incremental) {
-      claimableAmount = claimableAmount.add(
-        internalClaimByCall(holder, amountSince, sinceBlock, currentBlock)
-      );
+      claimableAmount = claimableAmount.add(internalClaimByCall(holder, amountSince, sinceBlock));
     }
 
-    if (claimableAmount > 0) {
-      address mintTo = receiver;
-      for (IRewardMinter minter = _rewardMinter; minter != IRewardMinter(0); ) {
-        (minter, mintTo) = minter.mintReward(mintTo, claimableAmount);
-      }
-      emit RewardsClaimed(holder, receiver, claimableAmount);
-    }
-    console.log('RewardsClaimed', claimableAmount);
     return claimableAmount;
   }
 
-  function internalCalcClaimableReward(
-    address holder,
-    uint256 mask,
-    uint32 currentBlock
-  ) private view returns (uint256 claimableAmount, uint256 delayedAmount) {
-    mask &= ~_ignoreMask;
-    mask &= _memberOf[holder];
-
+  function internalCalcClaimableReward(address holder, uint256 mask)
+    internal
+    view
+    override
+    returns (uint256 claimableAmount, uint256 delayedAmount)
+  {
     uint32 sinceBlock = 0;
     uint256 amountSince = 0;
     bool incremental = false;
@@ -279,7 +73,7 @@ abstract contract BasicRewardController is Ownable, MarketAccessBitmask, IManage
         continue;
       }
 
-      (uint256 amount_, uint32 since_) = _poolList[i].calcRewardFor(holder);
+      (uint256 amount_, uint32 since_) = getPool(i).calcRewardFor(holder);
       if (amount_ == 0) {
         continue;
       }
@@ -290,8 +84,7 @@ abstract contract BasicRewardController is Ownable, MarketAccessBitmask, IManage
       }
 
       if (amountSince > 0) {
-        (uint256 ca, uint256 da) =
-          internalCalcByCall(holder, amountSince, sinceBlock, currentBlock, incremental);
+        (uint256 ca, uint256 da) = internalCalcByCall(holder, amountSince, sinceBlock, incremental);
         claimableAmount = claimableAmount.add(ca);
         delayedAmount = delayedAmount.add(da);
         incremental = true;
@@ -301,8 +94,7 @@ abstract contract BasicRewardController is Ownable, MarketAccessBitmask, IManage
     }
 
     if (amountSince > 0 || !incremental) {
-      (uint256 ca, uint256 da) =
-        internalCalcByCall(holder, amountSince, sinceBlock, currentBlock, incremental);
+      (uint256 ca, uint256 da) = internalCalcByCall(holder, amountSince, sinceBlock, incremental);
       claimableAmount = claimableAmount.add(ca);
       delayedAmount = delayedAmount.add(da);
     }
@@ -310,38 +102,16 @@ abstract contract BasicRewardController is Ownable, MarketAccessBitmask, IManage
     return (claimableAmount, delayedAmount);
   }
 
-  function internalAllocatedByPool(
-    address holder,
-    uint256 allocated,
-    uint32 sinceBlock,
-    uint32 currentBlock
-  ) internal virtual;
-
   function internalClaimByCall(
     address holder,
     uint256 allocated,
-    uint32 sinceBlock,
-    uint32 currentBlock
+    uint32 sinceBlock
   ) internal virtual returns (uint256 amount);
 
   function internalCalcByCall(
     address holder,
     uint256 allocated,
     uint32 sinceBlock,
-    uint32 currentBlock,
     bool incremental
   ) internal view virtual returns (uint256 claimableAmount, uint256 delayedAmount);
-
-  modifier notPaused() {
-    require(!_paused, 'rewards are paused');
-    _;
-  }
-
-  function setPaused(bool paused) public override onlyEmergencyAdmin {
-    _paused = paused;
-  }
-
-  function isPaused() public view override returns (bool) {
-    return _paused;
-  }
 }
--- contracts/reward/RewardBooster.sol
+++ contracts/reward/RewardBooster.sol
@@ -5,70 +5,122 @@ import {SafeMath} from '../dependencies/openzeppelin/contracts/SafeMath.sol';
 import {PercentageMath} from '../tools/math/PercentageMath.sol';
 
 import {IMarketAccessController} from '../access/interfaces/IMarketAccessController.sol';
-import {BasicRewardController} from './BasicRewardController.sol';
+import {BaseRewardController} from './BaseRewardController.sol';
 import {IRewardMinter} from '../interfaces/IRewardMinter.sol';
+import {IRewardPool, IManagedRewardPool} from './interfaces/IRewardPool.sol';
 
 import 'hardhat/console.sol';
 
-contract RewardBooster is BasicRewardController {
+contract RewardBooster is BaseRewardController {
   using SafeMath for uint256;
   using PercentageMath for uint256;
+
+  mapping(address => uint256) private _boostFactor;
+  IManagedRewardPool private _boostPool;
 
   mapping(address => uint256) private _boostRewards;
   mapping(address => uint256) private _claimableRewards;
 
-  //  uint256 private _ignoreClaims;
-
   constructor(IMarketAccessController accessController, IRewardMinter rewardMinter)
     public
-    BasicRewardController(accessController, rewardMinter)
+    BaseRewardController(accessController, rewardMinter)
   {}
+
+  function setBoostFactor(address pool, uint32 pctFactor) external onlyOwner {}
+
+  function setBoostPool(address pool) external onlyOwner {}
+
+  function internalClaimAndMintReward(address holder, uint256 mask)
+    internal
+    override
+    returns (uint256 claimableAmount)
+  {
+    uint256 boostLimit = 0;
+
+    claimableAmount = _claimableRewards[holder];
+    if (claimableAmount > 0) {
+      delete (_claimableRewards[holder]);
+    }
+
+    for (uint256 i = 0; mask != 0; (i, mask) = (i + 1, mask >> 1)) {
+      if (mask & 1 == 0) {
+        continue;
+      }
+
+      IManagedRewardPool pool = getPool(i);
+      (uint256 amount_, ) = pool.claimRewardFor(holder);
+      if (amount_ == 0) {
+        continue;
+      }
+
+      claimableAmount = claimableAmount.add(amount_);
+      boostLimit = boostLimit.add(amount_.percentMul(_boostFactor[address(pool)]));
+    }
+
+    uint256 boost = _boostRewards[holder];
+    if (boost > 0) {
+      delete (_boostRewards[holder]);
+    }
+
+    if (_boostPool != IManagedRewardPool(0)) {
+      (uint256 boost_, ) = _boostPool.claimRewardFor(holder);
+      boost = boost.add(boost_);
+    }
+
+    if (boost <= boostLimit) {
+      claimableAmount = claimableAmount.add(boost);
+    } else {
+      claimableAmount = claimableAmount.add(boostLimit);
+      internalStoreBoostExcess(boost - boostLimit);
+    }
+
+    return claimableAmount;
+  }
+
+  function internalCalcClaimableReward(address holder, uint256 mask)
+    internal
+    view
+    override
+    returns (uint256 claimableAmount, uint256)
+  {
+    uint256 boostLimit = 0;
+
+    for (uint256 i = 0; mask != 0; (i, mask) = (i + 1, mask >> 1)) {
+      if (mask & 1 == 0) {
+        continue;
+      }
+
+      IManagedRewardPool pool = getPool(i);
+      (uint256 amount_, ) = pool.calcRewardFor(holder);
+      if (amount_ == 0) {
+        continue;
+      }
+
+      claimableAmount = claimableAmount.add(amount_);
+      boostLimit = boostLimit.add(amount_.percentMul(_boostFactor[address(pool)]));
+    }
+
+    uint256 boost = _boostRewards[holder];
+
+    if (_boostPool != IManagedRewardPool(0)) {
+      (uint256 boost_, ) = _boostPool.calcRewardFor(holder);
+      boost = boost.add(boost_);
+    }
+
+    if (boost <= boostLimit) {
+      claimableAmount = claimableAmount.add(boost);
+    } else {
+      claimableAmount = claimableAmount.add(boostLimit);
+    }
+
+    return (claimableAmount, 0);
+  }
 
   function internalAllocatedByPool(
     address holder,
     uint256 allocated,
-    uint32 sinceBlock,
-    uint32 currentBlock
-  ) internal override {
-    allocated = internalApplyAllocated(holder, allocated, sinceBlock, currentBlock);
-    if (allocated > 0) {
-      _claimableRewards[holder] = _claimableRewards[holder].add(allocated);
-    }
-  }
+    uint32 sinceBlock
+  ) internal override {}
 
-  function internalClaimByCall(
-    address holder,
-    uint256 allocated,
-    uint32 sinceBlock,
-    uint32 currentBlock
-  ) internal override returns (uint256 amount) {
-    amount = internalApplyAllocated(holder, allocated, sinceBlock, currentBlock);
-
-    uint256 claimableReward = _claimableRewards[holder];
-    if (claimableReward > 0) {
-      amount = amount.add(claimableReward);
-      delete (_claimableRewards[holder]);
-    }
-
-    return amount;
-  }
-
-  function internalApplyAllocated(
-    address holder,
-    uint256 allocated,
-    uint32 sinceBlock,
-    uint32 currentBlock
-  ) private returns (uint256) {
-    return 0;
-  }
-
-  function internalCalcByCall(
-    address holder,
-    uint256 allocated,
-    uint32 sinceBlock,
-    uint32 currentBlock,
-    bool incremental
-  ) internal view override returns (uint256 claimableAmount, uint256 frozenAmount) {
-    return (0, 0);
-  }
+  function internalStoreBoostExcess(uint256 boostExcess) private {}
 }
