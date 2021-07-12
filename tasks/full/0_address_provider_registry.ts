3ac8b99df70ef1c5ad65f57cdf999b1841aec9a7
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-07-12 18:29:28 +0300

    Deploy registry when not available

--- tasks/full/0_address_provider_registry.ts
+++ tasks/full/0_address_provider_registry.ts
@@ -1,11 +1,27 @@
 import { task } from 'hardhat/config';
+import { getParamPerNetwork } from '../../helpers/contracts-helpers';
 import { deployAddressesProviderRegistry } from '../../helpers/contracts-deployments';
+import { eNetwork } from '../../helpers/types';
+import { loadPoolConfig } from '../../helpers/configuration';
+import { isAddress } from 'ethers/lib/utils';
+import { isZeroAddress } from 'ethereumjs-util';
 
 task('full:deploy-address-provider-registry', 'Deploy address provider registry')
   .addFlag('verify', 'Verify contracts at Etherscan')
-  .setAction(async ({ verify }, DRE) => {
+  .setAction(async ({ verify, pool }, DRE) => {
     await DRE.run('set-DRE');
 
-    const contract = await deployAddressesProviderRegistry(verify);
-    console.log('Registry Address:', contract.address);
+    const network = <eNetwork>DRE.network.name;
+    const poolConfig = loadPoolConfig(pool);
+
+    let providerRegistryAddress = getParamPerNetwork(poolConfig.ProviderRegistry, network);
+
+    if (
+      !providerRegistryAddress ||
+      !isAddress(providerRegistryAddress) ||
+      isZeroAddress(providerRegistryAddress)
+    ) {
+      const contract = await deployAddressesProviderRegistry(verify);
+      console.log('Deployed Registry Address:', contract.address);
+    }
   });
