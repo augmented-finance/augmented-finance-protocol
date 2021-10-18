c59bbb545a765ade348ddcde48f21cea495f22c5
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-10-18 13:02:20 +0300

    Support for AGF roaming

---
+++ contracts/reward/AGFTokenV2.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.4;
+
+import '../access/AccessFlags.sol';
+import '../access/MarketAccessBitmask.sol';
+import '../access/AccessHelper.sol';
+import '../access/interfaces/IMarketAccessController.sol';
+import '../tools/tokens/ERC20BaseWithPermit.sol';
+import './interfaces/IRoamingToken.sol';
+import './AGFTokenV1.sol';
+
+contract AGFTokenV2 is AGFTokenV1, IRoamingToken {
+  uint256 private constant TOKEN_REVISION = 2;
+
+  int256 private _roamingSupply;
+
+  function getRevision() internal pure virtual override returns (uint256) {
+    return TOKEN_REVISION;
+  }
+
+  function burnToRoaming(address sender, uint256 amount)
+    external
+    override
+    aclHas(AccessFlags.REWARD_BRIDGE)
+    returns (RoamingData memory result)
+  {
+    require(amount > 0 && amount <= uint256(type(int256).max), 'INVALID_AMOUNT');
+
+    uint256 chainId;
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      chainId := chainid()
+    }
+
+    _burn(sender, amount);
+    _roamingSupply -= int256(amount);
+
+    result = RoamingData(amount, allocatedSupply(), chainId, block.timestamp, block.number);
+    emit BurnedToRoaming(sender, amount, result);
+    return result;
+  }
+
+  function mintFromRoaming(address receiver, RoamingData calldata data)
+    external
+    override
+    aclHas(AccessFlags.REWARD_BRIDGE)
+  {
+    require(data.amount > 0 && data.amount <= uint256(type(int256).max), 'INVALID_AMOUNT');
+    _mintReward(receiver, data.amount);
+    _roamingSupply += int256(data.amount);
+    emit MintedFromRoaming(receiver, data.amount, data);
+  }
+
+  function roamingSupply() external view override returns (int256) {
+    return _roamingSupply;
+  }
+}
---
+++ contracts/reward/interfaces/IRoamingToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.4;
+
+interface IRoamingToken {
+  struct RoamingData {
+    uint256 amount;
+    uint256 allocatedSupply;
+    uint256 fromChainId;
+    uint256 timestamp;
+    uint256 blockNumber;
+  }
+
+  event BurnedToRoaming(address indexed sender, uint256 amount, RoamingData data);
+  event MintedFromRoaming(address indexed receiver, uint256 amount, RoamingData data);
+
+  function burnToRoaming(address sender, uint256 amount) external returns (RoamingData memory);
+
+  function mintFromRoaming(address receiver, RoamingData calldata data) external;
+
+  function roamingSupply() external view returns (int256);
+}
