02aefaaf421e290dad44fae43baa4c6f83c02208
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-11-09 21:25:46 +0300

    Improve proxy

--- contracts/tools/upgradeability/TransparentProxyBase.sol
+++ contracts/tools/upgradeability/TransparentProxyBase.sol
@@ -7,33 +7,19 @@ import './IProxy.sol';
 
 /// @dev This contract is a transparent upgradeability proxy with admin. The admin role is immutable.
 abstract contract TransparentProxyBase is BaseUpgradeabilityProxy, IProxy {
-  bytes32 internal constant ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
+  address private immutable _admin;
 
   constructor(address admin) {
     require(admin != address(0));
     assert(IMPLEMENTATION_SLOT == bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1));
-    assert(ADMIN_SLOT == bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1));
-
-    bytes32 slot = ADMIN_SLOT;
-    // solhint-disable-next-line no-inline-assembly
-    assembly {
-      sstore(slot, admin)
-    }
+    _admin = admin;
   }
 
   modifier ifAdmin() {
-    if (msg.sender == _admin()) {
+    if (msg.sender == _admin) {
       _;
     } else {
       _fallback();
-    }
-  }
-
-  function _admin() internal view returns (address impl) {
-    bytes32 slot = ADMIN_SLOT;
-    // solhint-disable-next-line no-inline-assembly
-    assembly {
-      impl := sload(slot)
     }
   }
 
@@ -50,7 +36,7 @@ abstract contract TransparentProxyBase is BaseUpgradeabilityProxy, IProxy {
 
   /// @dev Only fall back when the sender is not the admin.
   function _willFallback() internal virtual override {
-    require(msg.sender != _admin(), 'Cannot call fallback function from the proxy admin');
+    require(msg.sender != _admin, 'Cannot call fallback function from the proxy admin');
     super._willFallback();
   }
 }
