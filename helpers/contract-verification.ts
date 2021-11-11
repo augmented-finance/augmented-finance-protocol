7cf62bb3b9afb942b92dea5d09a0b23f51d1b1a7
agAgent0xF8 <79453266+agAgent0xF8@users.noreply.github.com>
2021-11-11 13:03:07 +0300

    Support BscScan verification and separate keys (#228)

--- helpers/contract-verification.ts
+++ helpers/contract-verification.ts
@@ -80,25 +80,26 @@ export const verifyProxy = async (proxyAddr: string, implAddr: string): Promise<
 };
 
 const _verifyProxy = async (proxyAddr: string, implAddr: string) => {
-  //  console.log(`Verifying ${proxyName}...`)
-  const networkName = DRE.network.name;
-  const endpoints = await getEtherscanEndpoints(<EthereumProvider>(<any>getDefaultProvider(networkName)), networkName);
+  const endpoints = await getEtherscanEndpoints(DRE.network.provider, DRE.network.name);
   const apiKey = (<any>DRE.config).etherscan.apiKey!;
-  // const apiSubdomain = networkName === 'main' ? 'api' : `api-${networkName}`;
-  // const baseUrl = `https://${apiSubdomain}.etherscan.io/api?module=contract`;
   const baseUrl = `${endpoints.apiURL}?module=contract`;
 
   let guid: string;
   {
     const optionsVerify: AxiosRequestConfig = {
       method: 'POST',
+      url: baseUrl + `&action=verifyproxycontract&apikey=${apiKey}`,
       headers: { 'content-type': 'application/x-www-form-urlencoded' },
       data: qs.stringify({ address: proxyAddr, expectedimplementation: implAddr }),
-      url: baseUrl + `&action=verifyproxycontract&apikey=${apiKey}`,
     };
     const result = await axios(optionsVerify);
     const response = new EtherscanResponse(result.data);
     if (!response.isOk()) {
+      if (response.isProxyImplNotDetected()) {
+        if (await _verifyProxyByWebForm(endpoints.browserURL, proxyAddr, implAddr)) {
+          return;
+        }
+      }
       throw new Error(response.message);
     }
     guid = response.message;
@@ -119,6 +120,12 @@ const _verifyProxy = async (proxyAddr: string, implAddr: string) => {
       }
 
       if (!response.isPending()) {
+        if (response.isProxyImplNotDetected()) {
+          if (await _verifyProxyByWebForm(endpoints.browserURL, proxyAddr, implAddr)) {
+            return;
+          }
+        }
+
         throw Error(response.message);
       }
       if (i >= 20) {
@@ -155,7 +162,138 @@ class EtherscanResponse {
     return this.message.startsWith('Unable to locate ContractCode at');
   }
 
+  public isProxyImplNotDetected() {
+    // A corresponding implementation contract was unfortunately not detected for the proxy address
+    return this.message.startsWith('A corresponding implementation contract');
+  }
+
   public isOk() {
     return this.status === 1;
   }
 }
+
+let lastCallAt = 0;
+const defaultWebFormDelay = 3000; // millis
+
+const _verifyProxyByWebForm = async (endpoint: string, proxyAddr: string, implAddr: string): Promise<boolean> => {
+  const baseUrl = `${endpoint}/proxyContractChecker?a=${proxyAddr}`;
+  console.log(`\n\tVerifying proxy via the web form...`);
+
+  const fillForm = (s: string) => {
+    let form = {
+      __VIEWSTATE: '',
+      __VIEWSTATEGENERATOR: '',
+      __EVENTVALIDATION: '',
+      ctl00$ContentPlaceHolder1$txtContractAddress: proxyAddr,
+    };
+
+    const re = / id="(__(VIEWSTATE|VIEWSTATEGENERATOR|EVENTVALIDATION))" +value="([^"]+)"/g;
+
+    let m: RegExpExecArray | null;
+    while ((m = re.exec(s))) {
+      form[m[1]] = m[3];
+    }
+    if (!form.__EVENTVALIDATION || !form.__VIEWSTATE || !form.__VIEWSTATEGENERATOR) {
+      console.log(s);
+      throw new Error('Unable to find fields required for the web form');
+    }
+
+    return form;
+  };
+
+  let webFormDelay = defaultWebFormDelay;
+  const sendRequest = async (optionsVerify: AxiosRequestConfig, reqType: string) => {
+    for (let i = 10; i > 0; i--) {
+      {
+        const current = new Date().getTime();
+        const remains = lastCallAt + webFormDelay - current;
+        lastCallAt = current;
+        if (remains > 0) {
+          await sleep(remains);
+        }
+      }
+
+      const result = await axios(optionsVerify);
+      if (result.status != 200) {
+        console.log('Unexpected response:', reqType, result.status, result.statusText);
+        return undefined;
+      }
+      const s = <string>result.data;
+      if (s.indexOf('our servers are currently busy') < 0) {
+        return s;
+      }
+      webFormDelay += 10000;
+      console.log('\tServer is busy, wait for', webFormDelay, 'ms');
+    }
+    throw new Error('Too many retries for the web form');
+  };
+
+  let form;
+  {
+    const s = await sendRequest(
+      {
+        method: 'GET',
+        url: baseUrl,
+      },
+      'get'
+    ); // sampleGet;
+    if (!s) {
+      return false;
+    }
+    form = fillForm(s);
+  }
+
+  {
+    const req: AxiosRequestConfig = {
+      method: 'POST',
+      headers: {
+        Origin: endpoint,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Referer: baseUrl,
+      },
+      url: baseUrl,
+    };
+
+    const s = await sendRequest(
+      {
+        ...req,
+        data: qs.stringify({
+          ...form,
+          ctl00$ContentPlaceHolder1$btnSubmit: 'Verify',
+        }),
+      },
+      'verify'
+    ); // sampleVerify;
+    if (!s) {
+      return false;
+    }
+
+    const re = / implementation contract is found at: <a href='\/address\/(0x[0-9a-fA-F]+)'>/;
+    const m = re.exec(s);
+    if (!m) {
+      return false;
+    }
+
+    const foundImpl = m[1];
+    if (foundImpl.toLowerCase() != implAddr.toLowerCase()) {
+      throw new Error(`Proxy implementation mismatched: expected=${implAddr}, found=${foundImpl}`);
+    }
+
+    form = fillForm(s);
+    const s2 = await sendRequest(
+      {
+        ...req,
+        data: qs.stringify({
+          ...form,
+          ctl00$ContentPlaceHolder1$btnSubmitProxyDetails: 'Save',
+        }),
+      },
+      'save'
+    );
+    if (!s2 || s2.indexOf('Successfully saved') < 0) {
+      return false;
+    }
+
+    return true;
+  }
+};
