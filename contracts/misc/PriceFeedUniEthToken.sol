271aad07fcbe032bf1348ca407e2c42e5de6f990
agAgent013 <>
2021-11-29 17:58:55 +0300

    refactor: move rate calculation for uni eth token to a method

--- contracts/misc/PriceFeedUniEthToken.sol
+++ contracts/misc/PriceFeedUniEthToken.sol
@@ -11,10 +11,11 @@ contract PriceFeedUniEthToken is IPriceFeed {
 
   IUniswapV2Pair private immutable _token;
   address private immutable _underlying;
-  uint256 private immutable _decimalMul;
-  bool private immutable _baseAt1;
-
+  uint256 private immutable _quoteValue;
   uint32 private _lastUpdatedAt;
+  uint8 private immutable _decimals0;
+  uint8 private immutable _decimals1;
+  bool private immutable _baseAt1;
 
   constructor(
     address token,
@@ -26,28 +27,27 @@ contract PriceFeedUniEthToken is IPriceFeed {
     _token = IUniswapV2Pair(token);
     (address t0, address t1) = (IUniswapV2Pair(token).token0(), IUniswapV2Pair(token).token1());
     bool base1 = t1 == priceBase;
-    (uint256 decimalsT0, uint256 decimalsT1) = (IERC20Detailed(t0).decimals(), IERC20Detailed(t1).decimals());
-    uint256 decimals0 = base1 ? decimalsT0 : decimalsT1;
-    uint256 decimals1 = base1 ? decimalsT1 : decimalsT0;
+    (uint8 decimalsT0, uint8 decimalsT1) = (IERC20Detailed(t0).decimals(), IERC20Detailed(t1).decimals());
+    _decimals0 = base1 ? decimalsT0 : decimalsT1;
+    _decimals1 = base1 ? decimalsT1 : decimalsT0;
 
     if (!base1) {
       require(t0 == priceBase);
       t0 = t1;
     }
 
-    uint256 decimalsWithCorrection = (quoteValue * 10**decimals1) / 10**decimals0;
-    require(decimalsWithCorrection > 0);
-
-    _decimalMul = decimalsWithCorrection;
+    _quoteValue = quoteValue;
     _underlying = t0;
     _baseAt1 = base1;
   }
 
   function updatePrice() public override {
     (uint256 rate, uint32 timestamp) = currentPrice();
+
     if (_lastUpdatedAt == timestamp) {
       return;
     }
+
     _lastUpdatedAt = timestamp;
 
     emit DerivedAssetSourceUpdated(
@@ -63,20 +63,18 @@ contract PriceFeedUniEthToken is IPriceFeed {
 
   function currentPrice() private view returns (uint256, uint32) {
     (uint112 reserve0, uint112 reserve1, uint32 timestamp) = IUniswapV2Pair(_token).getReserves();
-    uint256 value;
-    if (_baseAt1) {
-      value = reserve0 > 0 ? (uint256(reserve1) * _decimalMul) / uint256(reserve0) : 0;
-    } else {
-      value = reserve1 > 0 ? (uint256(reserve0) * _decimalMul) / uint256(reserve1) : 0;
-    }
+    uint256 value = _baseAt1 ? _calculateRate(reserve0, reserve1) : _calculateRate(reserve1, reserve0);
+
     return (value, timestamp);
   }
 
   function latestAnswer() external view override returns (int256) {
     (uint256 rate, ) = currentPrice();
+
     if (rate >= uint256(type(int256).max)) {
       return type(int256).max;
     }
+
     return int256(rate);
   }
 
@@ -87,5 +85,12 @@ contract PriceFeedUniEthToken is IPriceFeed {
   function latestRound() external pure override returns (uint256) {
     // this value is checked by the OracleRouter to find out if updatePrice() should be called
     return type(uint256).max;
+  }
+
+  /**
+   * @dev Calculate `reserve` price expressed in `priceBase`
+   */
+  function _calculateRate(uint256 reserve, uint256 priceBase) private view returns (uint256) {
+    return reserve > 0 ? ((priceBase * _quoteValue * 10**_decimals1) / 10**_decimals0) / reserve : 0;
   }
 }
